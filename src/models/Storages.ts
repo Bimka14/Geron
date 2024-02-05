109c652e5faae79e9e7cb6446814caad1d6e8475
Your Name <you@example.com>
2024-02-05 19:03:35 +0600

    14

--- src/models/Storages.ts
+++ src/models/Storages.ts
@@ -1,38 +1,38 @@
-import  storages from '../data/storages.json';
-import { TProducts } from './Products';
-export type TStorages = {
-    city: string,
-    name: string,
-    address: string,
-    id: number,
-    products: TProducts[]
-}
+// import  storages from '../data/storages.json';
+// import { TProducts } from './Products';
+// export type TStorages = {
+//     city: string,
+//     name: string,
+//     address: string,
+//     id: number,
+//     products: TProducts[]
+// }
 
-export class Storages{
-    private _data: TStorages[]
+// export class Storages{
+//     private _data: TStorages[]
 
-    constructor() {
-        this._data = storages;
-    }
+//     constructor() {
+//         this._data = storages;
+//     }
 
-    get allStorages(){
-        return this._data
-    }
+//     get allStorages(){
+//         return this._data
+//     }
 
-    protected getStorage(id:number):TStorages {
-        return this._data.find((item=>{ item.id===id}))
-    }
+//     protected getStorage(id:number):TStorages {
+//         return this._data.find((item=>{ item.id===id}))
+//     }
 
-    private set updateStorage(storage:TStorages) {
-        this._data.map((item)=>{
-            if(item.id !== storage.id)return item
-            return storage
-        })
-    }
+//     private set updateStorage(storage:TStorages) {
+//         this._data.map((item)=>{
+//             if(item.id !== storage.id)return item
+//             return storage
+//         })
+//     }
 
-    protected setProducts(id:number, product:TProducts){
-        const storage = this.getStorage(id)
-        storage.products.push(product)
-        this.updateStorage = storage
-    }
-}
+//     protected setProducts(id:number, product:TProducts){
+//         const storage = this.getStorage(id)
+//         storage.products.push(product)
+//         this.updateStorage = storage
+//     }
+// }
